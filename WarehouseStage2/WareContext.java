6a198932087c031d45e2ab4edfbdf93cdaf146d3
SalmaanShire365 <salmaanshire@gmail.com>
2025-10-30 13:37:09 -0500

    Added WareContext + Loginstate

---
+++ WarehouseStage2/WareContext.java
@@ -0,0 +1,141 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.text.*;
+import java.io.*;
+public class WareContext {
+  
+  private static int currentState;
+  private static Warehouse warehouse;
+  private static WareContext context;
+  private int currentUser;
+  private String userID;
+  private static JFrame LibFrame; 
+  private BufferedReader reader = new BufferedReader(new 
+                                      InputStreamReader(System.in));
+  public static final int IsClerk = 0;
+  public static final int IsUser = 1;
+  private static WareState[] states;
+  private int[][] nextState;
+
+  public String getToken(String prompt) {
+    do {
+      try {
+        System.out.println(prompt);
+        String line = reader.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(line,"\n\r\f");
+        if (tokenizer.hasMoreTokens()) {
+          return tokenizer.nextToken();
+        }
+      } catch (IOException ioe) {
+        System.exit(0);
+      }
+    } while (true);
+  }
+  
+  private boolean yesOrNo(String prompt) {
+    String more = getToken(prompt + " (Y|y)[es] or anything else for no");
+    if (more.charAt(0) != 'y' && more.charAt(0) != 'Y') {
+      return false;
+    }
+    return true;
+  }
+
+  private void retrieve() {
+    try {
+      Warehouse tempWarehouse = Warehouse.retrieve();
+      if (tempWarehouse != null) {
+        System.out.println(" The warehouse has been successfully retrieved from the file WarehouseData \n" );
+        warehouse = tempWarehouse;
+      } else {
+        System.out.println("File doesnt exist; creating new warehosue" );
+        warehouse = Warehouse.instance();
+      }
+    } catch(Exception cnfe) {
+      cnfe.printStackTrace();
+    }
+  }
+
+  public void setLogin(int code)
+  {currentUser = code;}
+
+  public void setUser(String uID)
+  { userID = uID;}
+
+  public int getLogin()
+  { return currentUser;}
+
+  public String getUser()
+  { return userID;}
+
+  public JFrame getFrame()
+  { return WareFrame;}
+
+  private WareContext() { //constructor
+    System.out.println("In Warecontext constructor");
+    if (yesOrNo("Look for saved data and  use it?")) {
+      retrieve();
+    } else {
+      warehouse = Warehouse.instance();
+    }
+    // set up the FSM and transition table;
+    states = new WareState[3];
+    states[0] = Clerkstate.instance();
+    states[1] = Userstate.instance(); 
+    states[2]=  Loginstate.instance();
+    nextState = new int[3][3];
+    nextState[0][0] = 2;nextState[0][1] = 1;nextState[0][2] = -2;
+    nextState[1][0] = 2;nextState[1][1] = 0;nextState[1][2] = -2;
+    nextState[2][0] = 0;nextState[2][1] = 1;nextState[2][2] = -1;
+    currentState = 2;
+    WareFrame = new JFrame("Warehouse GUI");
+	WareFrame.addWindowListener(new WindowAdapter()
+       {public void windowClosing(WindowEvent e){System.exit(0);}});
+    WareFrame.setSize(400,400);
+    WareFrame.setLocation(400, 400);
+    }
+
+  public void changeState(int transition)
+  {
+    //System.out.println("current state " + currentState + " \n \n ");
+    currentState = nextState[currentState][transition];
+    if (currentState == -2) 
+      {System.out.println("Error has occurred"); terminate();}
+    if (currentState == -1) 
+      { //System.out.println("current state " + currentState + " \n \n ");
+      terminate();}
+    states[currentState].run();
+  }
+
+  private void terminate()
+  {
+   if (yesOrNo("Save data?")) {
+      if (warehouse.save()) {
+         System.out.println(" The warehouse has been successfully saved in the file WarehouseData \n" );
+       } else {
+         System.out.println(" There has been an error in saving \n" );
+       }
+     }
+   System.out.println(" Goodbye \n "); System.exit(0);
+  }
+
+  public static WareContext instance() {
+    if (context == null) {
+       System.out.println("calling constructor");
+      context = new WareContext();
+    }
+    return context;
+  }
+
+  public void process(){
+    states[currentState].run();
+  }
+  
+  public static void main (String[] args){
+    WareContext.instance().process(); 
+    states[currentState].run();
+  }
+
+
+}
